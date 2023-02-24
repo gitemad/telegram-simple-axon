4ff184c710d2cb8f88c6b8dd218aa5507cec6511
Emad <sudani.emad@gmail.com>
2023-02-24 14:03:09 +0330

    init project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Avatar  :user="user"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Avatar from '@/components/Avatar.vue';
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    Avatar
+  },
+  data() {
+    return {
+      user: {
+        name: 'Emad',
+      },
+    }
+  },
 }
+
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,45 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+      loaderOptions: {
+          sass: {
+            additionalData: `
+                  @import "@/assets/scss/variables.scss";
+              `
+          }
+      },
+      extract: {
+        filename: 'css/[name].css',
+        chunkFilename: 'css/[name].css'
+      }
+  },
+  configureWebpack:{
+		optimization: {
+			runtimeChunk: 'single',
+			splitChunks: {
+				cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+				},
+			},
+		},
+		output: {
+			filename: 'js/[name].js',
+			chunkFilename: 'js/[name].js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+	},
+  
+	pwa: {
+		name: 'Telegram Simple',
+		themeColor: '#0088CC',
+		msTileColor: '#FFFFFF',
+		manifestOptions: {
+			background_color: '#FFFFFF'
+		}
+	}
 })
